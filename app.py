d4fb1bf3ec7a66aca0e8743aa82b3146111bd8f8
Robinson Marquez <robinsonmadrid@outlook.com>
2024-11-25 20:29:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -11,22 +11,8 @@ def build_routes():
 
     app = Flask(__name__)
 
-    @app.route('/201')
-    def hello201():
-        return "Hello, course 201!"
-
-    @app.route('/random-boolean')
-    def random_boolean():
-        value = random.choice([True, False])
-        return jsonify(result=value)
-
-    # Route (/201/cat)
-    @app.route('/201/cat')
-    def cat():
-        dummy_data = {"name": "Whiskers", "age": 3,
-                      "breed": "Siamese", "color": "Brown"}
-        response = jsonify(dummy_data)
-        response.headers.add('Access-Control-Allow-Origin', '*')
-        return response
+    @app.route('/home')
+    def home():
+        return "Hello from home"
 
     return app
